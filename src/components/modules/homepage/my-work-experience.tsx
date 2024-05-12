523601202ab58c4414107e3b4bc65953fd928628
Muhammad Arifanjas Rachmattullah <arifanjas.code@gmail.com>
2024-05-12 15:47:39 +0700

    new

---
+++ src/components/modules/homepage/my-work-experience.tsx
@@ -0,0 +1,87 @@
+export const ModuleHomepageMyWorkExperiences = () => {
+    return (
+        <div className="px-[100px] py-[148px] flex items-center justify-center bg-genoa-50">
+            <ol className="overflow-hidden space-y-10">
+                <li className="relative flex">
+                    <div className="flex items-start font-medium w-full">
+                        <div className="w-[220px] mr-20 space-y-2">
+                            <h4 className="text-xl font-semibold">Inovasika</h4>
+                            <p className="text-base-black font-normal  leading-8 tracking-wider ">
+                                Nov 2020 - Mar 2021
+                                <br />
+                            </p>
+                        </div>
+                        <span className="w-12 h-12 aspect-square bg-genoa-600 border-8 border-genoa-50 z-50 rounded-full flex justify-center items-center mr-3 text-sm text-white after:content-[''] after:w-0.5 after:h-full after:bg-genoa-600 after:inline-block after:absolute after:-bottom-12"></span>
+                        <div className="block ml-20">
+                            <h4 className="text-xl font-semibold mb-2">
+                                Graphic Designer
+                            </h4>
+                            <p className="text-base-black font-normal max-w-3xl mb-4 leading-8 tracking-wider">
+                                A Graphic Designer for create visual materials
+                                that company&apos;s marketing or communication
+                                needs, including print and digital media, such
+                                as brochures, posters, logos and other
+                                promotional materials.
+                                <br />
+                            </p>
+                        </div>
+                    </div>
+                </li>
+
+                <li className="relative flex">
+                    <div className="flex items-start font-medium w-full">
+                        <div className="w-[220px] mr-20 space-y-2">
+                            <h4 className="text-xl font-semibold">Inovasika</h4>
+                            <p className="text-base-black font-normal  leading-8 tracking-wider ">
+                                Mar 2021 - Agu 2023
+                                <br />
+                            </p>
+                        </div>
+                        <span className="w-12 h-12 aspect-square bg-outrageous-orange-600 border-8 border-genoa-50 z-50 rounded-full flex justify-center items-center mr-3 text-sm text-white after:content-[''] after:w-0.5 after:h-full after:bg-outrageous-orange-600 after:inline-block after:absolute after:-bottom-12"></span>
+                        <div className="block ml-20">
+                            <h4 className="text-xl font-semibold mb-2">
+                                UI/UX Designer
+                            </h4>
+                            <p className="text-base-black font-normal max-w-3xl mb-4 leading-8 tracking-wider">
+                                A Graphic Designer for design attractive UI UX
+                                in applications or websites. They focus on
+                                design elements, website products, and
+                                interactivity to increase user comfort and
+                                satisfaction.
+                                <br />
+                            </p>
+                        </div>
+                    </div>
+                </li>
+
+                <li className="relative flex">
+                    <div className="flex items-start font-medium w-full">
+                        <div className="w-[220px] mr-20 space-y-2">
+                            <h4 className="text-xl font-semibold">
+                                Terralogiq
+                            </h4>
+                            <p className="text-base-black font-normal  leading-8 tracking-wider ">
+                                Agu 2023 - Current
+                                <br />
+                            </p>
+                        </div>
+                        <span className="w-12 h-12 aspect-square bg-smoky-600 border-8 border-genoa-50 z-50 rounded-full flex justify-center items-center mr-3 text-sm text-white "></span>
+                        <div className="block ml-20">
+                            <h4 className="text-xl font-semibold mb-2">
+                                UI/UX Designer
+                            </h4>
+                            <p className="text-base-black font-normal  leading-8 tracking-wider max-w-3xl mb-4">
+                                UI UX Designer for my career here, I design
+                                interactive UI and create UX flows that make it
+                                easier for users, focusing on process design,
+                                system design, interactive maps, monitoring
+                                dashboards, etc.
+                                <br />
+                            </p>
+                        </div>
+                    </div>
+                </li>
+            </ol>
+        </div>
+    );
+};
